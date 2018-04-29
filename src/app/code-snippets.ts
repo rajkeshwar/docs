02df6048f77b12e88d01e301dfdc35355f5b5539
Travis CI User <travis@example.org>
2018-04-29 03:23:00 +0000

    Auto-generated commit -- Cleaned up md-treeview.js
    Triggered by commit: https://github.com/rajkeshwar/docs/commit/e2ee7c3e1e33acbefd5b104213dd1cf21d00726f
    Travis build: https://travis-ci.org/rajkeshwar/docs/builds/372584263

--- src/app/code-snippets.ts
+++
@@ -1,9 +0,0 @@
-
-declare var require:any;
-export const CODE_SNIPPETS={
-"./docs/file-1.md":require('./docs/file-1.md'),
-"./docs/file-2.md":require('./docs/file-2.md'),
-"./docs/file-3.md":require('./docs/file-3.md'),
-"./docs/folder-1/file-1.1.md":require('./docs/folder-1/file-1.1.md'),
-"./docs/folder-1/file-1.2.md":require('./docs/folder-1/file-1.2.md'),
-}
